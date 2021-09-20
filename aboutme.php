bdfac8236d49bd85ba5e8ce8da631089cd937695
Pratik Kumar Patil <80801716+Garuda5611@users.noreply.github.com>
2021-09-20 11:35:43 +0530

    Add files via upload

---
+++ aboutme.php
@@ -0,0 +1,36 @@
+<?php include 'nav.php' ?>
+<head>
+<style>
+.mypic
+{
+padding:20px;
+}
+.name
+{
+padding:20px;
+}
+.detail
+{
+padding:20px;
+}
+</style>
+</head>
+<body>
+<center>
+<div class="mypic">
+<img src="images/mypic.png" width="200" height="200">
+</div>
+<div class="name">
+<h2>Pratik Patil</h2>
+</div>
+<div class="detail">
+<p>Motivated and hard-working CSE undergraduate student with experience of working on web application development and software development. Interested in research oriented work. Currently trying to explore Data Science and Machine Learning fields. Projects in C, C++, Java, Python, HTML, CSS, JavaScript, JSP, VB .Net, Networking, Security, Data Science and Machine Learning. Dedicated towards achieving goals.</p>
+</center><ul>
+<li>Degree- BE in Computer Engineering</li>
+<li>Institute- Pimpri Chinchwad College of Engineering, Pune</li>
+<li>Portfolio- <a href="https://pratikpatil5611.github.io/portfolio/">https://pratikpatil5611.github.io/portfolio/</a></li>
+<li>LinkedIn- <a href="https://www.linkedin.com/in/pratik-patil-79463117b/">https://www.linkedin.com/in/pratik-patil-79463117b/</a></li>
+</ul>
+</div>
+</body>
+<?php include 'footer.php' ?>
